263a62bf2c1d01e1a0704f8a78044f9f2cc6bdbc
Hong WooBeom <hob0961@naonsoft.com>
2022-12-11 21:46:33 +0900

    Initial commit

---
+++ src/main/java/com/youngsanimax/YoungsanImaxApplication.java
@@ -0,0 +1,13 @@
+package com.youngsanimax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungsanImaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoungsanImaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youngsanimax/YoungsanIamxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngsanimax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungsanIamxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
