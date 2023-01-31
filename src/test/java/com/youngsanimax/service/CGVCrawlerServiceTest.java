8f2ccb7cae5047b98067b77285805ad964cc4c14
w-beom <w_bum@naver.com>
2023-02-01 01:36:57 +0900

    테스트코드 수정

--- src/test/java/com/youngsanimax/service/CGVCrawlerServiceTest.java
+++ src/test/java/com/youngsanimax/service/CGVCrawlerServiceTest.java
@@ -13,30 +13,5 @@ import java.time.format.DateTimeFormatter;
 
 class CGVCrawlerServiceTest {
     @Test
-    void crawling() throws InterruptedException {
-        String areaCode = "01";
-        String theaterCode = "0013";
-        String today = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-        String url = "http://www.cgv.co.kr/theaters/?areacode=" + areaCode + "&theaterCode=" + theaterCode + "&date=" + today;
-
-        Browser browser = new Chrome();
-        WebDriver webDriver = browser.getWebDriver();
-        webDriver.get(url);
-
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
-        javascriptExecutor.executeScript("document.getElementsByClassName('descri-info')[0].remove();");
-
-        webDriver.switchTo().frame(3);
-
-        int sliderSize = webDriver.findElement(By.id("slider")).findElements(By.className("item-wrap")).size();
-        for (int i = 1; i <= sliderSize; i++) {
-            WebElement element = webDriver.findElement(By.xpath("//*[@id=\"slider\"]/div[" + i + "]"));
-            int dateSize = element.findElements(By.tagName("li")).size();
-            for (int j = 1; j <= dateSize; j++) {
-                WebElement day = webDriver.findElement(By.xpath("//*[@id=\"slider\"]/div[" + i + "]/ul/li[" + j + "]")); // 날짜 클릭
-                day.click();
-            }
-            webDriver.findElement(By.xpath("//*[@id=\"slider\"]/button[2]")).click(); // 다음날짜보기
-        }
-    }
+    void crawling() throws InterruptedException {}
 }
