3a661f34cb9fae89cab450eb4890c83ef84cba58
w-beom <w_bum@naver.com>
2023-02-04 23:43:15 +0900

    리팩토링

--- src/main/java/com/youngsanimax/service/CrawlerService.java
+++ src/main/java/com/youngsanimax/service/CrawlerService.java
@@ -3,5 +3,5 @@ package com.youngsanimax.service;
 import com.youngsanimax.domain.browser.Browser;
 
 public interface CrawlerService {
-    void crawling(Browser browser) throws InterruptedException;
+    void crawling(Browser browser);
 }
